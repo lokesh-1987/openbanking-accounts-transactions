41e4477ac9290061bf9a9fbe91f89592ff2dfccd
2020-DEV-051 <2020.DEV.051@gmail.com>
2020-05-23 17:05:10 +0530

    Added main class in src folder

---
+++ src/main/java/com/openbanking/accounts/transactions/AccountTransactionApplication.java
@@ -0,0 +1,12 @@
+package com.openbanking.accounts.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountTransactionApplication.class, args);
+    }
+}
